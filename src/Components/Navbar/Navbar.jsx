3eddb1255664eb6d490a0997e988934bae2f7332
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-20 10:07:55 +0530

    build navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navContainer">
-        <span className='logo'></span>
+        
+        <span className='logo'> logo </span>
         <div className="navItems">
               <button className="navButton">Register</button>
               <button className="navButton">Login</button>
