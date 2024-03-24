2efdd0f17d88f32a94d8db28915fd9b98181a67c
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-24 18:37:28 +0530

    css style

---
+++ src/Components/FeaturedProperties/FeaturedProperties.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './FeaturedProperties.css'
+import img from '../../assets/im9.webp'
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+       <div className="fpItem">
+          <img src={img} alt="" />
+          <span className="fpName">aptrrts Hotel</span>
+          <span className="fpCity">Madrid</span>
+          <span className="fpPrice">Startting form $200</span>
+          <div className="fpRating">
+              <button>8.9</button>
+              <span>Excellent</span>
+           </div>
+       </div>
+       <div className="fpItem">
+          <img src={img} alt="" />
+          <span className="fpName">aptrrts Hotel</span>
+          <span className="fpCity">Madrid</span>
+          <span className="fpPrice">Startting form $200</span>
+          <div className="fpRating">
+              <button>8.9</button>
+              <span>Excellent</span>
+           </div>
+       </div>
+       <div className="fpItem">
+          <img src={img} alt="" />
+          <span className="fpName">aptrrts Hotel</span>
+          <span className="fpCity">Madrid</span>
+          <span className="fpPrice">Startting form $200</span>
+          <div className="fpRating">
+              <button>8.9</button>
+              <span>Excellent</span>
+           </div>
+       </div>
+       <div className="fpItem">
+          <img src={img} alt="" />
+          <span className="fpName">aptrrts Hotel</span>
+          <span className="fpCity">Madrid</span>
+          <span className="fpPrice">Startting form $200</span>
+          <div className="fpRating">
+              <button>8.9</button>
+              <span>Excellent</span>
+           </div>
+       </div> 
+
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../Components/Navbar/Navbar'
 import Header from '../../Components/Header/Header'
 import Featured from '../../Components/Featured/Featured'
 import PropertyList from '../../Components/PropertyList/PropertyList'
+import FeaturedProperties from '../../Components/FeaturedProperties/FeaturedProperties'
 
 const Home = () => {
   return (
@@ -15,7 +16,7 @@ const Home = () => {
         <h1 className="homeTitle"> Browse Property By Type</h1>
         <PropertyList/>
         <h1 className="homeTitle"> Home guests love</h1>
-
+        <FeaturedProperties/>
       </div>
       
     </div>
