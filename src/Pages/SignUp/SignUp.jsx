f15ecaf9029f3ee5943ff5c67aeb7a679ce69053
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-29 15:16:57 +0530

    created otp page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -7,10 +7,14 @@ const SignUp = () => {
             <div className="containerLogin">
                 <section className="wrapperLogin">
                     <div className="headingLogin">
-                        <h1 className="text text-large">Sign In</h1>
+                        <h1 className="text text-large">Sign Up</h1>
                         <p className="text text-normal">New user? <span><a href="#" className="text text-links">Create an account</a></span></p>
                     </div>
                     <form name="signin" className="formLogin">
+                        <div className="input-control">
+                            <label htmlFor="" className="input-label" hidden>UserName</label>
+                            <input type="email" name="email" id="email" className="input-field" placeholder="UserName" />
+                        </div>
                         <div className="input-control">
                             <label htmlFor="email" className="input-label" hidden>Email Address</label>
                             <input type="email" name="email" id="email" className="input-field" placeholder="Email Address" />
@@ -21,7 +25,7 @@ const SignUp = () => {
                         </div>
                         <div className="input-control">
                             <a href="#" className="text text-links">Forgot Password</a>
-                            <input type="submit" name="submit" id="input-submit" value="Sign In" disabled />
+                            <input type="submit" name="submit" id="input-submit" value="Sign In"/>
                         </div>
                     </form>
                     <div className="striped">
