fef49b8e9d8fbc50326d31a12ff524aa9161df56
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-30 12:13:26 +0530

    build error message for login and signup

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Login/Login.css'
+import axios from 'axios'
 
 const SignUp = () => {
+
+    const [userName,setUsername]  = useState('')        
+    const [email,setEmail]  = useState('')        
+    const [passwrd,setPassword]  = useState('')        
+    const [error,setError] = useState()
+
+    //handle signup function
+    const handleSignUp = () =>{
+        axios.post('',{
+            username:userName,
+            email:email,
+            password:password
+        }).then((response)=>{
+            console.log(response);
+        }).catch(err => setError(err))
+    }
+
     return (
         <main className="mainLogin">
             <div className="containerLogin">
@@ -12,26 +30,26 @@ const SignUp = () => {
                     </div>
                     <form name="signin" className="formLogin">
                         <div className="input-control">
-                            <label htmlFor="" className="input-label" hidden>UserName</label>
+                            <label htmlFor="" className="input-label" hidden onChange={(e)=>{setUsername(e.target.value)}}>UserName</label>
                             <input type="email" name="email" id="email" className="input-field" placeholder="UserName" />
                         </div>
                         <div className="input-control">
-                            <label htmlFor="email" className="input-label" hidden>Email Address</label>
+                            <label htmlFor="email" className="input-label" hidden onChange={(e)=>{setEmail(e.target.value)}}>Email Address</label>
                             <input type="email" name="email" id="email" className="input-field" placeholder="Email Address" />
                         </div>
                         <div className="input-control">
-                            <label htmlFor="password" className="input-label" hidden>Password</label>
+                            <label htmlFor="password" className="input-label" hidden onChange={(e)=>{setPassword(e.target.value)}}>Password</label>
                             <input type="password" name="password" id="password" className="input-field" placeholder="Password" />
                         </div>
                         <div className="input-control">
-                            <a href="#" className="text text-links">Forgot Password</a>
-                            <input type="submit" name="submit" id="input-submit" value="Sign In"/>
+                            <a href="#" className="text text-links">Already a user</a>
+                            <input type="submit" name="submit" id="input-submit" value="Sign Up" onClick={()=>{handleSignUp}}/>
                         </div>
                     </form>
-                    <div className="striped">
-                        <span className="striped-line"></span>
-                        <span className="striped-text"></span>
-                        <span className="striped-line"></span>
+                    <div className="loginMessage">
+                     {
+                      error && (<span style={{marginLeft:'10px'}}>Login Failed</span>)
+                     }
                     </div>
                 </section>
             </div>
