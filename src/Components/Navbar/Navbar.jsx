d70dfdfb0d915bcee5dce8ccce1c73b9d185212b
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-04-03 16:04:02 +0530

    build logout section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import './Navbar.css'
 import { useNavigate } from 'react-router-dom'
 
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+
 const Navbar = ({access}) => {
+
+  const cookies = new Cookies();
+
   const navigate = useNavigate()
+
+
+  const clearCookie = (name) => {
+  try {
+    cookies.remove(name, {path: "/", domain: "localhost"});
+  } catch (error) {
+    console.error('Error clearing cookie:', error);
+  }
+};
+
   return (
     <div>
       {!access ? (
@@ -25,6 +42,14 @@ const Navbar = ({access}) => {
         <div className="navItems">
               <button className="navButton" onClick={()=>{navigate('/')}}>My Bookings</button>
               <button className="navButton" onClick={()=>{navigate('/')}}>Profile</button>
+              <button className="navButton" onClick={async()=>{
+              //  Cookie.remove('access_tocken')
+              //  clearCookie('access_tocken');
+               await axios.post('http://localhost:3000/clearCookie','',{withCredentials:true}).then(()=>{
+                navigate('/login')
+               }).catch(err=>console.log(err))
+               
+                }}>LogOut</button>
         </div>
       </div>
     </div>
--- src/Pages/token.jsx
+++ src/Pages/token.jsx
@@ -1,14 +1,4 @@
 import axios from "axios";
-import Cookies from 'js-cookie';
-
-const cookieName = 'access_tocken';
-const cookieValue = Cookies.get(cookieName); 
-
-if (cookieValue) {
-    console.log("Cookie value found:", cookieValue);
-} else {
-    console.log("Cookie not found or inaccessible.");
-}
 
 const sampleUrl="http://localhost:3000"
 
