272d152821cc5959d0f92bbca18755480f5d048f
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-04-09 10:57:51 +0530

    finished hotels numbers fetching

--- src/Components/PropertyList/PropertyList.jsx
+++ src/Components/PropertyList/PropertyList.jsx
@@ -7,19 +7,19 @@ import useFetch from '../../hooks/useFetch'
 const PropertyList = () => {
 
        const {data,loading,error} = useFetch('http://localhost:3000/hotels/countByType')
-       console.log(data+'@@@@@@@@@@@');
+       
        const images  = [img9,img9,img9,img9,img9] 
     
   return (
    <>
-   {false ? ("laoding"):(
+   {loading ? ("laoding"):(
         <div className="pList">
-       {images.map((image,index)=>
+       {data && images.map((image,index)=>
         <div className="pListItem" key={index}>
                <img src={image} alt="" className="pListImg" />
                <div className="pListTitles">
-                      <h1>hotels</h1>
-                      <h2>2333 Rooms</h2>
+                      <h1>{data[index]?.type}</h1>
+                      <h2>{data[index]?.count} Rooms</h2>
                </div>
         </div>
        )}
