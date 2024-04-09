50dbea6cabcbff977dfdcc6a43f103c0e074adca
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-04-09 14:26:09 +0530

    fetched featured property list

--- src/Components/FeaturedProperties/FeaturedProperties.jsx
+++ src/Components/FeaturedProperties/FeaturedProperties.jsx
@@ -2,58 +2,26 @@ import React from 'react'
 import './FeaturedProperties.css'
 import useFetch from '../../hooks/useFetch'
 import img1 from '../../assets/ByTypeImages/hamburg-2976711_1280.jpg'
-import img2 from '../../assets/ByTypeImages/hamburg-3071437_1280.jpg'
-import img3 from '../../assets/ByTypeImages/hamburg-4103411_1280.jpg'
-import img4 from '../../assets/ByTypeImages/hamburg-5974223_1280.jpg'
-
 
 const FeaturedProperties = () => {
 
-   const {data,loading,error} = useFetch('http://localhost:3000/hotels/countByType')
+   const {data,loading,error} = useFetch('http://localhost:3000/hotels')
 
   return (
     <div className="fp">
+      {loading ? ('Loading') : (
+      data.map((data)=>
        <div className="fpItem">
           <img className='fpImg' src={img1} alt="" />
-          <span className="fpName">aptrrts Hotel</span>
-          <span className="fpCity">Madrid</span>
-          <span className="fpPrice">Startting form $200</span>
+          <span className="fpName">{data.name}</span>
+          <span className="fpCity">{data.city}</span>
+          <span className="fpPrice">Startting from ${data.cheapestPrice}</span>
           <div className="fpRating">
-              <button>8.9</button>
-              <span>Excellent</span>
+              <button>{data.rating}</button>
+              <span>{data.rating > 4.4 ? 'Excellent' : "Good"}</span>
            </div>
        </div>
-       <div className="fpItem">
-          <img className='fpImg' src={img2} alt="" />
-          <span className="fpName">aptrrts Hotel</span>
-          <span className="fpCity">Madrid</span>
-          <span className="fpPrice">Startting form $200</span>
-          <div className="fpRating">
-              <button>8.9</button>
-              <span>Excellent</span>
-           </div>
-       </div>
-       <div className="fpItem">
-          <img  className='fpImg' src={img3} alt="" />
-          <span className="fpName">aptrrts Hotel</span>
-          <span className="fpCity">Madrid</span>
-          <span className="fpPrice">Startting form $200</span>
-          <div className="fpRating">
-              <button>8.9</button>
-              <span>Excellent</span>
-           </div>
-       </div>
-       <div className="fpItem">
-          <img  className='fpImg'  src={img4} alt="" />
-          <span className="fpName">aptrrts Hotel</span>
-          <span className="fpCity">Madrid</span>
-          <span className="fpPrice">Startting form $200</span>
-          <div className="fpRating">
-              <button>8.9</button>
-              <span>Excellent</span>
-           </div>
-       </div> 
-
+      ))}
     </div>
   )
 }
