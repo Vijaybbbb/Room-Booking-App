2983dccd915bbadb757ae37a6a9de4566ef86b72
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-04-09 13:58:10 +0530

    css updations

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -11,42 +11,23 @@ const Featured = () => {
 
  
   const {data,loading,error} = useFetch('http://localhost:3000/hotels/countByCity?cities=berlin,madrid,london')
-  const images  = []
-
+  const images  = [img1,img2,img3,img4]
+  const places  = ['Mumbai','Chennai','Banglore','Delhi'] 
   return (
     <div>
       {loading ? (
       'Loading'
       ):(
       <div className="featured">
+        {images && images.map((img,index)=>
        <div className="featuredItem">
-              <img src={img1} alt="" className="featuredImg" />
+              <img src={img} alt="" className="featuredImg" />
               <div className="featuredTitles">
-                <h1>ooty</h1>
-                <h2>{data[0]} Properties</h2>
+                <h1 style={{position:'relative',marginBottom:'2px'}}>{places[index]}</h1>
+                <h2 style={{position:'relative',marginBottom:'33px'}}>{data[index]} Properties</h2>
               </div>
        </div>
-       <div className="featuredItem">
-              <img src={img2} alt="" className="featuredImg" />
-              <div className="featuredTitles">
-                <h1>ooty</h1>
-                <h2>{data[1]} Properties</h2>
-              </div>
-       </div>
-       <div className="featuredItem">
-              <img src={img3} alt="" className="featuredImg" />
-              <div className="featuredTitles">
-                <h1>ooty</h1>
-                <h2>{data[2]} Properties</h2>
-              </div>
-       </div>
-       <div className="featuredItem">
-              <img src={img4} alt="" className="featuredImg" />
-              <div className="featuredTitles">
-                <h1>ooty</h1>
-                <h2>{data[1]} Properties</h2>
-              </div>
-       </div>
+      )}
       </div>
 )} 
   </div>
