1a0e2e8889b92a92364b5203e7b066b25889e67f
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-04-17 13:01:31 +0530

    build pagination for users list

--- src/Pages/admin/AdminComponents/UserManagement.jsx
+++ src/Pages/admin/AdminComponents/UserManagement.jsx
@@ -1,16 +1,106 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../adminHome/AdminHome.css'
+import '../AdminComponents/css/UserManagement.css'
+import axios from 'axios'
+import useFetch from '../../../hooks/useFetch'
+import {baseUrl} from '../../../utils'
+import ViewUser from './ViewUser'
 
 const UserManagement = ({compClick,showUsers}) => {
 
   const [openWindow,setOpenWindow] = useState(false)
- 
+  const {data,error,loading} = useFetch(`${baseUrl}/admin/users`)
+  const [page, setPage] = useState(1)
+  const [viewUser,setViewUser] = useState(false)
+  const [userId, setUserId] = useState()
+
+  const TableRow = ({ children }) => {
+    return <div className="table__row">{children}</div>;
+  };
+  const TableCell = ({ children }) => {
+    return <div className="table__cell">{children}</div>;
+  };
+
+  //function for indicate pagination
+  function selectedPage(selectedPage) {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= data.length &&
+      selectedPage !== page
+    )
+      setPage(selectedPage)
+  }
+  
+  async function handleViewUser(userId){
+    setViewUser(true)
+    setUserId(userId)
+  }
+  
+  
   return (
-      <div>
-        {
-          showUsers ? (
-            <div>user information</div>
-          ):(
+    <div>
+      {
+        showUsers ? (
+          <div className='allUsersTable'>
+            <div className="table">
+              <div className="table__body">
+                <TableRow>
+                  <TableCell>User</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>UserID</TableCell>
+                  <TableCell></TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+                {/* Repeat TableRow and TableCell components for each row */}
+                {/* Example of a row */}
+                {
+                  data && data.slice((page - 1) * 5, page * 5).map(user=>(
+                    <TableRow>
+                  <TableCell>
+                    <h3 className="table__crypto-name">{user?.username}</h3>
+                  </TableCell>
+                  <TableCell><input type="text" value={user?.email} className='detailsshowinput' readOnly /></TableCell>
+                    
+                  <TableCell><input type="text" value={user?._id} className='detailsshowinput' readOnly /></TableCell>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="buttons">
+                      <button className="button button--outline buttons__venta" onClick={()=>{handleViewUser(user._id)}}>View User</button>
+                      <button className="button button--primary buttons__comprar">Edit User</button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+                  ))
+                }
+                 <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+
+                <TableCell colSpan="5">
+                  <div style={{alignItems:'center'}}>
+                  <div className="page-btn">
+                    <span onClick={() => selectedPage(page - 1)}>{'<'}</span>
+                    {[...Array(Math.ceil(data.length / 5))].map((_, i) => (
+                      <span
+                        key={i + 1}
+                        onClick={() => selectedPage(i + 1)}
+                        className={page === i + 1 ? 'pagination_selected' : ''}
+                      >{i + 1}</span>
+                    ))}
+                    <span onClick={() => selectedPage(page + 1)}>{'>'}</span>
+                  </div>
+                  </div>
+                </TableCell>
+              </TableRow>
+                {/* Repeat this structure for each row */}
+              </div>
+            </div>
+          </div>
+          ):
+          viewUser?(
+              <ViewUser id={userId}/>
+          )
+          :(
             <div className="col s6" onClick={()=>{compClick(event,'UserManagement')}}>
        <div style={{ padding: '35px' }} align="center" className="card">
          <div className="row">
